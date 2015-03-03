380f0402764c106a1e5a46d23744582e7ee7bae2
adrianvinuelas <adrian.vinuelas@gmail.com>
2015-03-03 22:56:58 +0100

    modificado lista de tres filtros

--- 5.6.8.js
+++ 5.6.8.js
@@ -1,33 +1,81 @@
 var datos = "";
-var filt ="fuenlabrada";
+var filt1 ="";
+var filt2 ="";
+var filt3 ="";
 jQuery(document).ready(function() {
 	console.log("arrancado el dom");
 	//bajo el JSON y lo guardo en la global datos para usarlo despues
-    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+filt+"&tagmode=any&format=json&jsoncallback=?",function(data){
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+filt1+"&tagmode=any&format=json&jsoncallback=?",function(data){
 			datos = data;
 	});
 
-    $('#busqueda').html(filt);
-
     $('#filtrar').click(function(){
-    	console.log("entro a filtro");
-    	console.log($('#filtro').val());
-        if($('#filtro').val()!=""){
-            filt = $('#filtro').val();
+
+        if($('#filtro1').val()!=""){
+            filt1 = $('#filtro1').val();
+            $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+filt1+"&tagmode=any&format=json&jsoncallback=?",function(data){
+                datos = data;
+                cogerImagenes(1);
+            });
+        }else{
+            $('#fotos1').html("");
+            $('#busqueda1').html("");
+            $('#filtro1').val("");
         }
-        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+filt+"&tagmode=any&format=json&jsoncallback=?",function(data){
-            datos = data;
-            cogerImagenes();
-        });        
+
+        if($('#filtro2').val()!=""){
+            filt2 = $('#filtro2').val();
+            $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+filt2+"&tagmode=any&format=json&jsoncallback=?",function(data){
+                datos = data;
+                cogerImagenes(2);
+            });
+        }else{
+            $('#fotos2').html("");
+            $('#busqueda2').html("");
+            $('#filtro2').val("");
+        }
+
+        if($('#filtro3').val()!=""){
+            filt3 = $('#filtro3').val();
+            $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+filt3+"&tagmode=any&format=json&jsoncallback=?",function(data){
+                datos = data;
+                cogerImagenes(3);
+            });
+        }else{
+            $('#fotos3').html("");
+            $('#busqueda3').html("");
+            $('#filtro3').val("");
+        }
+                
     });
-    function cogerImagenes(){
-        var imagenes = "";
-        for(i=0;i<4;i++){
-            imagenes += "<img src="+ datos.items[i].media.m +"><br>";
+
+    function cogerImagenes(num){
+        console.log("entro a cogerimagenes con num = " + num);
+        if(num == 1){
+            var imagenes1 = "";
+            for(i=0;i<4;i++){
+                imagenes1 += "<img src="+ datos.items[i].media.m +"><br>";
+            }
+            $('#fotos1').html(imagenes1);
+            $('#busqueda1').html(filt1);
+            $('#filtro1').val("");
+        }else if(num == 2){
+            var imagenes2 = "";
+            for(i=0;i<4;i++){
+                imagenes2 += "<img src="+ datos.items[i].media.m +"><br>";
+            }
+            $('#fotos2').html(imagenes2);
+            $('#busqueda2').html(filt2);
+            $('#filtro2').val("");
+        }else if(num == 3){
+            var imagenes3 = "";
+            for(i=0;i<4;i++){
+                imagenes3 += "<img src="+ datos.items[i].media.m +"><br>";
+            }
+            $('#fotos3').html(imagenes3);
+            $('#busqueda3').html(filt3);
+            $('#filtro3').val("");
         }
-        $('#fotos').html(imagenes);
-        $('#busqueda').html(filt);
-        $('#filtro').val("");
     }
     
 });
